17e5fbfdc83e0ca64c3d54a218fa35c58df72a14
MrKarlLogan <mrelectricguitar7@gmail.com>
2026-03-26 19:07:12 +0300

    Поправлен адаптив. Теперь грузится 6 карточек.

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -4,7 +4,9 @@ export const useUsers = () => {
   return useQuery({
     queryKey: ["users"],
     queryFn: async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const res = await fetch(
+        "https://jsonplaceholder.typicode.com/users?_limit=6",
+      );
       return res.json();
     },
   });
